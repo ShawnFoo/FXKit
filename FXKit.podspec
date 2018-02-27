ee0f8e7e66147ddc644d08623e60f7dbab34dbb6
fuxiang <fuxiang@lvmama.com>
2018-02-27 18:27:54 +0800

    修改Public文件引入

--- FXKit.podspec
+++ FXKit.podspec
@@ -2,19 +2,16 @@ Pod::Spec.new do |spec|
   spec.name         = "FXKit"
   spec.version      = "0.1"
   spec.summary      = "Encapsulation of convenient utils."
-
   spec.homepage     = "https://github.com/ShawnFoo/FXKit"
   spec.license      = { :type => "MIT", :file => "LICENSE" }
-
   spec.author             = { "Shawn Foo" => "fu4904@gmail.com" }
   spec.platform     = :ios
   spec.ios.deployment_target = "8.0"
-
   spec.source       = { :git => "https://github.com/ShawnFoo/FXKit.git", :tag => "#{spec.version.to_s}" }
   spec.framework  = "UIKit"
   spec.requires_arc = true
 
-  spec.source_files = "FXKit/FXKitPublicHeader.h"
+  spec.source_files = "FXKit/FXKit.h"
 
   spec.subspec 'Util' do |ss|
     ss.source_files = "FXKit/Util/*.{h,m}"
--- FXKit/FXKit.h
+++ FXKit/FXKit.h
@@ -8,14 +8,44 @@
 
 #import <UIKit/UIKit.h>
 
+#if __has_include(<FXKit/FXKit.h>)
+
 //! Project version number for FXKit.
 FOUNDATION_EXPORT double FXKitVersionNumber;
 
 //! Project version string for FXKit.
 FOUNDATION_EXPORT const unsigned char FXKitVersionString[];
 
-#import <FXKit/FXKitPublicHeader.h>
+#import <FXKit/CADisplayLink+FXWeakTarget.h>
 
+#import <FXKit/NSArray+FXRearrange.h>
+#import <FXKit/NSTimer+FXWeakTarget.h>
+#import <FXKit/NSUserDefaults+FXSynchronizeSetter.h>
 
+#import <FXKit/UIView+FXOverlayCornerRadius.h>
 
+#import <FXKit/FXReuseObjectQueue.h>
+#import <FXKit/FXDeallocMonitor.h>
 
+#import <FXKit/FXDateUtil.h>
+#import <FXKit/FXFileUtil.h>
+#import <FXKit/FXPageUtil.h>
+
+#else
+
+#import "CADisplayLink+FXWeakTarget.h"
+
+#import "NSArray+FXRearrange.h"
+#import "NSTimer+FXWeakTarget.h"
+#import "NSUserDefaults+FXSynchronizeSetter.h"
+
+#import "UIView+FXOverlayCornerRadius.h"
+
+#import "FXReuseObjectQueue.h"
+#import "FXDeallocMonitor.h"
+
+#import "FXDateUtil.h"
+#import "FXFileUtil.h"
+#import "FXPageUtil.h"
+
+#endif
